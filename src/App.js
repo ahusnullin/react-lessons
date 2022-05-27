5ec5f94056d311624bdf813f3321b4a85926cc7e
ahusnullin <a.husnullin@mm94.ru>
2022-05-27 23:02:40 +0300

    Урок 2

--- src/App.js
+++ src/App.js
@@ -1,35 +1,58 @@
+import React, {useCallback, useEffect, useState} from "react";
 
-import {Message} from './components/Message'
+import style from "./common.module.less"
+import {Message} from "./components/Message/Message";
+import {Form} from "./components/Form/Form";
 
-import "./index.css"
+export const App = () => {
+    const [messagesList, setMessagesList] = useState([]);
 
-function App() {
+    useEffect(() => {
+        setMessagesList(
+            [
+                {
+                    author: 'Ваня',
+                    text: 'Привет от Вани!',
+                },
+                {
+                    author: 'Вася',
+                    text: 'Привет от Васи!',
+                },
+                {
+                    author: 'Петя',
+                    text: 'Привет от Пети!',
+                },
+                {
+                    author: 'Света',
+                    text: 'Привет от Светы!',
+                }
+            ]
+        )
+    }, []);
 
-    const messagesList = [
-        "Первое сообщение",
-        "Второе сообщение",
-        "Третье сообщение",
-    ];
-
-  return (
-    <div className="app">
-
-        <h2>
-            Одно сообщение:
-        </h2>
-
-        <Message text="Привет! Это тестовое сообщение" />
-
-        <h2>
-            Список:
-        </h2>
-
-        {messagesList.map((item, index) => <Message text={item} user_num={index} key={index}/>)}
+    useEffect(() => {
+        if (messagesList.length && messagesList.slice(-1)[0]?.author !== 'Bot') {
+            setTimeout(() => {
+                setMessagesList([ ...messagesList, {
+                    author: 'Bot',
+                    text: `Это автоматический ответ для: ${messagesList.slice(-1)[0]?.author}`,
+                } ])
+            }, 1500);
+        }
+    }, [messagesList]);
 
 
-        <div style={{ clear: 'both' }}></div>
+    const sendMessage = (messageObj) => {
+        setMessagesList([ ...messagesList, messageObj ])
+    }
+
+    return <div className={style.wrapper}>
+        <div className={style.content}>
+            {messagesList.map((data, index) => <Message data={data} key={index}/>)}
+            <Form sendMessage={sendMessage}/>
+            <div style={{ clear: 'both'}}></div>
+        </div>
+
+
     </div>
-  );
 }
-
-export default App;
