5a93b1cd84772488955a8532dfe0a5e6b6759702
ahusnullin <a.husnullin@mm94.ru>
2022-06-02 18:22:04 +0300

    Корректировки по итогам ревью

--- src/App.js
+++ src/App.js
@@ -1,35 +1,25 @@
-
 import {Message} from './components/Message'
-
-import "./index.css"
+import "./app.css"
 
 function App() {
-
     const messagesList = [
         "Первое сообщение",
         "Второе сообщение",
         "Третье сообщение",
     ];
-
-  return (
-    <div className="app">
-
-        <h2>
-            Одно сообщение:
-        </h2>
-
-        <Message text="Привет! Это тестовое сообщение" />
-
-        <h2>
-            Список:
-        </h2>
-
-        {messagesList.map((item, index) => <Message text={item} user_num={index} key={index}/>)}
-
-
-        <div style={{ clear: 'both' }}></div>
-    </div>
-  );
+    return (
+        <div className="app">
+            <h2>
+                Одно сообщение:
+            </h2>
+            <Message text="Привет! Это тестовое сообщение"/>
+            <h2>
+                Список:
+            </h2>
+            {messagesList.map((item, index) => <Message text={item} user_num={index} key={index}/>)}
+            <div style={{clear: 'both'}} />
+        </div>
+    );
 }
 
 export default App;
